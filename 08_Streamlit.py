b0963431839d32e8e4524303535daf2f0db031e3
Alfonso Nieto García <43706214+ANG112@users.noreply.github.com>
2024-07-08 20:48:30 +0200

    streamlit

---
+++ 08_Streamlit.py
@@ -0,0 +1,29 @@
+
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# ------ INCOMINGS -----
+
+
+# ------ SET_CONFING_PAGE-------
+st.set_page_config(
+    page_title='Customer_Engineering',
+    page_icon=':💶:',
+
+)
+
+st.write('Hello World')
+
+
+
+
+
+
+
+
+
+# ------- SIDEBAR ---------
+st.sidebar.selectbox('')
